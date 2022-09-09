3e0df17168a6c438a131a95e41650a8183073ed9
pc <test@test.com>
2022-09-09 14:12:45 +0530

    .

---
+++ app/src/main/java/com/example/astra/db/SessionManager.java
@@ -0,0 +1,15 @@
+package com.example.astra.db;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SessionManager {
+    SharedPreferences preferences;
+//    public static final StoreSetupActivityMvpView INSTANCE = ;
+
+
+    //Preff keys
+
+}
---
+++ app/src/main/java/com/example/astra/ui/main/AstraMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.astra.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.astra.R;
+
+public class AstraMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_astra_main);
+    }
+}
