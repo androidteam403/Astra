09b0861e3c95066b1dde06eb5741fa546fa68b72
pc <androidteam403>
2022-10-14 16:25:17 +0530

    .

--- app/src/main/java/com/thresholdsoft/astra/ui/login/LoginActivityCallback.java
+++ app/src/main/java/com/thresholdsoft/astra/ui/login/LoginActivityCallback.java
@@ -1,4 +1,8 @@
 package com.thresholdsoft.astra.ui.login;
 
+import com.thresholdsoft.astra.ui.login.model.ValidateUserModelResponse;
+
 public interface LoginActivityCallback {
+
+   void onSucessfullValidateResponse(ValidateUserModelResponse body);
 }
